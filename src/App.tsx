50132ef5352f52da656c650105ed53045fa248e1
Janardhan-21 <janardhanmaddineni@gmail.com>
2025-09-21 09:00:06 +0530

    Update files with improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 import Dashboard from './components/Dashboard/Dashboard';
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-  );
-};
+const App: React.FC = () => (
+  <Router>
+    <nav>
+      <Link to="/">Home</Link> |{' '}
+      <Link to="/login">Login</Link> |{' '}
+      <Link to="/register">Register</Link> |{' '}
+      <Link to="/dashboard">Dashboard</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
